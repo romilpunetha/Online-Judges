3f1692f5fb960434fbfa5b4b657bea8f554efc55
romilpunetha <punetharomil@gmail.com>
2017-02-20 17:40:13 +0530

    20-Feb-2017

--- Misc/max_set.cpp
+++ Misc/max_set.cpp
@@ -15,7 +15,7 @@
 #define S set
 #define UM unordered_map
 #define US unordered_set
-#define Graph list<int>*
+#define Graph map<int, int> *
 #define tr1(x)                cerr << #x << ": " << x << endl;
 #define tr2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
 #define tr3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
@@ -29,8 +29,54 @@ typedef unsigned long long ull;
 typedef double dbl;
 typedef long double ldbl;
 
+Graph g;
+UM<int, US<int>> mp1;
+S<V<V<int> > >res;
+V<int>tmp, visited;
+V<V<int> >ans;
+S<int> neg;
+
+void dfs(int u){
+    tmp.pb(u);
+    visited[u] = 1;
+    for(auto &v : g[u]){
+        if(!visited[v.ff] && v.ss >= 0 && !present(neg, v.ff)){
+            for(auto &it : mp1[v.ff]) neg.insert(it);
+            dfs(v.ff);
+        }
+    }
+}
+
+void util(int i){
+    neg.clear();
+    for(auto &it : mp1[i]) neg.insert(it);
+    if(!visited[i]) dfs(i);
+    sort(all(tmp));
+    if(!tmp.empty()) ans.pb(tmp);
+    tmp.clear();
+}
+
 int main(){
     ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    int n, m; cin >> n >> m; // n = no of nodes, m = no. of edges
+    g = new M<int, int>[n + 1];
+    while(m--){  // taking all edges from u to v with cost w as input
+        int u, v, w; cin >> u >> v >> w;
+        if( !g[u][v] || w < g[u][v] ) g[u][v] = w, g[v][u] = w; // creating graph
+        if(w < 0) mp1[u].insert(v), mp1[v].insert(u); // keeping track of -ve edges
+    }
+    for(int i = 1; i <= n; i++){
+        visited.clear(); visited.resize(n + 1, 0);
+        util(i); // running dfs from root node to get the main branch
+        for(auto j = 1; j <= n; j++) util(j); // running dfs from remaining nodes to get the leftover sets
+        res.insert(ans); ans.clear();
+    }
+    for(auto &it : res){
+        for(auto &jt : it){
+            for(auto &kt : jt) cout << kt << " "; cout << "    ";
+        }
+        cout << endl;
+    }
     return 0;
 }
 
