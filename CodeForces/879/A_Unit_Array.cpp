32be488f171a8dc470e808b43a2aa3ef8f51e678
Romil Punetha <romil.punetha@dstreet.finance>
2023-08-01 18:37:12 +0100

    c2_1600 20 questions

---
+++ CodeForces/879/A_Unit_Array.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define inf INT_MAX
+#define pb push_back
+#define der(c, x) ((c).find(x) != (c).end())
+#define base 999983
+#define baseinv 943912055
+#define ff first
+#define ss second
+#define V vector
+#define Vi V<int>
+#define VVi V<V<int>>
+#define Vll V<ll>
+#define L list
+#define P pair
+#define MP map
+#define ST set
+#define UM unordered_map
+#define MM multimap
+#define UMM unordered_multimap
+#define MST multiset
+#define UST unordered_set
+#define UMS unordered_multiset
+#define PQ priority_queue
+#define Pii P<int, int>
+#define Pll P<long long, long long>
+#define Graph V<L<int>>
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+#define stoi stoll
+#define si(x) int(x.size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end()), x.shrink_to_fit()
+#define tr1(x) cerr << #x << ": " << x << endl;
+#define tr2(x, y) cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define tr3(x, y, z) cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define tr4(a, b, c, d) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define tr5(a, b, c, d, e) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define tr6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+using dbl = double;
+using ldbl = long double;
+
+int popcnt(int x) { return __builtin_popcount(x); }
+int popcnt(u32 x) { return __builtin_popcount(x); }
+int popcnt(ll x) { return __builtin_popcountll(x); }
+int popcnt(u64 x) { return __builtin_popcountll(x); }
+// (0, 1, 2, 3, 4) -> (-1, 0, 1, 1, 2)
+int topbit(int x) { return (x == 0 ? -1 : 31 - __builtin_clz(x)); }
+int topbit(u32 x) { return (x == 0 ? -1 : 31 - __builtin_clz(x)); }
+int topbit(ll x) { return (x == 0 ? -1 : 63 - __builtin_clzll(x)); }
+int topbit(u64 x) { return (x == 0 ? -1 : 63 - __builtin_clzll(x)); }
+// (0, 1, 2, 3, 4) -> (-1, 0, 1, 0, 2)
+int lowbit(int x) { return (x == 0 ? -1 : __builtin_ctz(x)); }
+int lowbit(u32 x) { return (x == 0 ? -1 : __builtin_ctz(x)); }
+int lowbit(ll x) { return (x == 0 ? -1 : __builtin_ctzll(x)); }
+int lowbit(u64 x) { return (x == 0 ? -1 : __builtin_ctzll(x)); }
+
+template <typename T, typename U>
+T ceil(T x, U y) {
+    return (x > 0 ? (x + y - 1) / y : x / y);
+}
+template <typename T, typename U>
+T floor(T x, U y) {
+    return (x > 0 ? x / y : (x - y + 1) / y);
+}
+template <typename T, typename U>
+pair<T, T> divmod(T x, U y) {
+    T q = floor(x, y);
+    return { q, x - q * y };
+}
+
+template <typename T, typename U>
+T SUM(const vector<U> &A) {
+    T sum = 0;
+    for (auto &&a : A) sum += a;
+    return sum;
+}
+
+template <typename A, typename B>
+inline ostream &operator<<(ostream &os, const pair<A, B> &v) { return os << v.first << ' ' << v.second; }
+template <typename A>
+inline ostream &operator<<(ostream &os, const vector<A> &v) {
+    auto it = v.begin();
+    os << *it;
+    for (++it; it != v.end();
+         os << ' ' << *it++)
+        ;
+    return os;
+}
+void tr() { cout << endl; }
+template <typename H, typename... T>
+inline void tr(H head, T... tail) {
+    cerr << head << ' ';
+    tr(tail...);
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    int p = 0, q = 0;
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        if (t == 1)
+            p++;
+        else
+            q++;
+    }
+    int cnt = 0, sum = p - q;
+    while (sum < 0) {
+        p++, q--, cnt += 1;
+        sum = p - q;
+    }
+    if (q % 2 == 0)
+        cout << cnt << endl;
+    else
+        cout << cnt + 1 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
---
+++ CodeForces/881/E_Tracking_Segments.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define inf INT_MAX
+#define pb push_back
+#define der(c, x) ((c).find(x) != (c).end())
+#define base 999983
+#define baseinv 943912055
+#define ff first
+#define ss second
+#define V vector
+#define Vi V<int>
+#define VVi V<V<int>>
+#define Vll V<ll>
+#define L list
+#define P pair
+#define MP map
+#define ST set
+#define UM unordered_map
+#define MM multimap
+#define UMM unordered_multimap
+#define MST multiset
+#define UST unordered_set
+#define UMS unordered_multiset
+#define PQ priority_queue
+#define Pii P<int, int>
+#define Pll P<long long, long long>
+#define Graph V<L<int>>
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+#define stoi stoll
+#define si(x) int(x.size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end()), x.shrink_to_fit()
+#define tr1(x) cerr << #x << ": " << x << endl;
+#define tr2(x, y) cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define tr3(x, y, z) cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define tr4(a, b, c, d) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define tr5(a, b, c, d, e) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define tr6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+using dbl = double;
+using ldbl = long double;
+
+int popcnt(int x) { return __builtin_popcount(x); }
+int popcnt(u32 x) { return __builtin_popcount(x); }
+int popcnt(ll x) { return __builtin_popcountll(x); }
+int popcnt(u64 x) { return __builtin_popcountll(x); }
+// (0, 1, 2, 3, 4) -> (-1, 0, 1, 1, 2)
+int topbit(int x) { return (x == 0 ? -1 : 31 - __builtin_clz(x)); }
+int topbit(u32 x) { return (x == 0 ? -1 : 31 - __builtin_clz(x)); }
+int topbit(ll x) { return (x == 0 ? -1 : 63 - __builtin_clzll(x)); }
+int topbit(u64 x) { return (x == 0 ? -1 : 63 - __builtin_clzll(x)); }
+// (0, 1, 2, 3, 4) -> (-1, 0, 1, 0, 2)
+int lowbit(int x) { return (x == 0 ? -1 : __builtin_ctz(x)); }
+int lowbit(u32 x) { return (x == 0 ? -1 : __builtin_ctz(x)); }
+int lowbit(ll x) { return (x == 0 ? -1 : __builtin_ctzll(x)); }
+int lowbit(u64 x) { return (x == 0 ? -1 : __builtin_ctzll(x)); }
+
+template <typename T, typename U>
+T ceil(T x, U y) {
+    return (x > 0 ? (x + y - 1) / y : x / y);
+}
+template <typename T, typename U>
+T floor(T x, U y) {
+    return (x > 0 ? x / y : (x - y + 1) / y);
+}
+template <typename T, typename U>
+pair<T, T> divmod(T x, U y) {
+    T q = floor(x, y);
+    return { q, x - q * y };
+}
+
+template <typename T, typename U>
+T SUM(const vector<U> &A) {
+    T sum = 0;
+    for (auto &&a : A) sum += a;
+    return sum;
+}
+
+template <typename A, typename B>
+inline ostream &operator<<(ostream &os, const pair<A, B> &v) { return os << v.first << ' ' << v.second; }
+template <typename A>
+inline ostream &operator<<(ostream &os, const vector<A> &v) {
+    auto it = v.begin();
+    os << *it;
+    for (++it; it != v.end();
+         os << ' ' << *it++)
+        ;
+    return os;
+}
+void tr() { cout << endl; }
+template <typename H, typename... T>
+inline void tr(H head, T... tail) {
+    cerr << head << ' ';
+    tr(tail...);
+}
+
+bool check(V<Pii> &arr, Vi &queries, int mid, int n) {
+    Vi brr(n + 1, 0);
+    for (int i = 0; i <= mid; i++) brr[queries[i]] = 1;
+    for (int i = 1; i <= n; i++) brr[i] += brr[i - 1];
+    for (int i = 0; i < arr.size(); i++) {
+        int required = ((arr[i].ss - arr[i].ff + 1) / 2) + 1;
+        if (brr[arr[i].ss] - brr[arr[i].ff - 1] >= required) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    V<Pii> arr(m);
+    for (auto &it : arr) cin >> it.ff >> it.ss;
+    int q;
+    cin >> q;
+    Vi queries(q);
+    for (auto &it : queries) cin >> it;
+    int start = 0, end = q - 1, mid;
+    while (start < end) {
+        mid = (start + end) / 2;
+        if (check(arr, queries, mid, n))
+            end = mid;
+        else
+            start = mid + 1;
+    }
+    if (check(arr, queries, start, n))
+        cout << start + 1 << endl;
+    else
+        cout << -1 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
